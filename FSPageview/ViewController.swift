a5c625866aecfb903253cf80b4ff56d1bb59ee23
Jeevan <jeevanchelachodan@gmail.com>
2023-03-15 12:00:45 +0530

    Initial Commit

---
+++ FSPageview/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  FSPageview
+//
+//  Created by RP-14 on 15/03/23.
+//
+
+import UIKit
+import FSPagerView
+
+class ViewController: UIViewController,FSPagerViewDelegate,FSPagerViewDataSource {
+
+    @IBOutlet weak var fspage: FSPagerView!
+    
+ let imgarray = ["img1","img2","img3","img4","img5"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.fspage.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+        fspage.transformer = FSPagerViewTransformer(type:.invertedFerrisWheel)
+        fspage.delegate = self
+        fspage.dataSource = self
+        fspage.automaticSlidingInterval = 2.0
+    }
+
+    func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return imgarray.count
+    }
+    
+    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index) as! FSPagerViewCell
+        cell.textLabel?.text = "Text:\(index)"
+        cell.imageView?.image = UIImage(named: imgarray[index])
+        return cell
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FSPageview' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FSPageview
+pod 'FSPagerView', '~> 0.8'
+end
